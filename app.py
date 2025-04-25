1ff7bcce2dcc50013909b97d5ccead7f117a24fb
yaashwin <muhammedyaashwin@gmail.com>
2025-04-25 12:06:53 +0530

    first commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World from Flask!"
+    return "Hello, World from Flasks!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
