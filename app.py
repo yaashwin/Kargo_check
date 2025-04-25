a7f63c39c7dfcef5f81c2cc814785c7c5e69ff07
yaashwin <muhammedyaashwin@gmail.com>
2025-04-25 12:28:41 +0530

    checking versio

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World from yaashwin C!"
+    return "Hello, World from yaashwin!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
