c705e98c495fc099d8e17a710c65ffe14da0088a
yaashwin <144644407+yaashwin@users.noreply.github.com>
2025-04-29 13:43:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "my name is yaashwin."
+    return "my name is yaashwin c."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
